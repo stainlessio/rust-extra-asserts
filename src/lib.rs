e5a5ed5e9f6de0b83812f1d09c38a3600a8b34f9
Russell Hay <RussTheAerialist@users.noreply.github.com>
2018-06-03 15:32:03 -0400

    Adding travis-ci configuration (#1)
    * Adding travis-ci configuration
    * Address clippy failures
    * cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::ops::Sub;
 use std::fmt::Debug;
+use std::ops::Sub;
 
 /// Asserts if a floating point is within some epsilon.  This allows
 /// you to compare calculations to make sure it's within some error factor
@@ -10,7 +10,7 @@ use std::fmt::Debug;
 /// use extra_asserts::assert_approx_eq;
 /// let x : f64 = 10.123456789;
 /// let y : f64 = 10.123467890;
-/// assert_approx_eq(x, y, 1e-4);
+/// assert_approx_eq(x, y, &1e-4);
 /// ```
 ///
 /// This one should fail
@@ -18,15 +18,13 @@ use std::fmt::Debug;
 /// use extra_asserts::assert_approx_eq;
 /// let x : f64 = 10.123456789;
 /// let y : f64 = 10.123467890;
-/// assert_approx_eq(x, y, 1e-10);
+/// assert_approx_eq(x, y, &1e-10);
 /// ```
-pub fn assert_approx_eq<T>(l : T, r : T, epsilon : T::Output)
-where T : Sub + PartialOrd + Debug + Copy,
-T::Output : Debug + PartialOrd {
-    let diff = if l < r {
-        r - l
-    } else {
-        l - r
-    };
-    assert!(diff < epsilon, format!("{:?} != {:?}", l, r));
+pub fn assert_approx_eq<T>(l: T, r: T, epsilon: &T::Output)
+where
+    T: Sub + PartialOrd + Debug + Copy,
+    T::Output: Debug + PartialOrd,
+{
+    let diff = if l < r { r - l } else { l - r };
+    assert!(diff < *epsilon, format!("{:?} != {:?}", l, r));
 }
